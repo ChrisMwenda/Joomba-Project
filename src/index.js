9b15359c278ef668dcf8b33f7c8e9bbf91a857c0
Christopher <chrismbogo97@gmail.com>
2022-09-13 02:34:23 +0300

    layout changes

--- src/index.js
+++ src/index.js
@@ -2,8 +2,41 @@ document.addEventListener('DOMContentLoaded',init())
 
 function init(){
   activateFAQ();
-  toggleTeamBio()
+  toggleTeamBio();
+  toggleUseCase();
 }
+
+/*Use Cases*/
+function toggleUseCase(){
+  const button= document.querySelector('checked')
+  console.log(button)
+}
+  
+  /*const body= document.getElementsByClassName('use-case-body')
+  console.log(body)
+
+  for (let x=0;x<button.length;a++){
+    teamMember[a].addEventListener('click',function(){
+     console.log(teamMember[a])
+     const bio=document.createElement('div')
+     bio.classList.add('bio')
+     if(teamMember[a].textContent==='Chris Mwenda'){
+     bio.textContent="Chris is an Entrepreneur by birth. WIth a bachelors degree in business and years long experience in real estate, he is the brains behind this organisation "
+     }
+     if(teamMember[a].textContent==='Willie Mbanya'){
+       bio.textContent="Willie is an product manager per excellence.Over the years he ahs been designing and building attractive looking apps. He's the hipster."
+       }
+     if(teamMember[a].textContent==='Jonathan Munyi'){
+        bio.textContent="Jonathan is the architect, orchestrating how things happen behind the scenes. He's the hacker who makes sure Joomba's engines run so smoothly"
+         }
+     role=document.getElementsByClassName('role')
+     role[a].after(bio)
+     console.log(bio)
+    
+           }
+          )}*/
+
+
 /*Form Code Ativation*/
 /*
 function activateForm(){
@@ -54,41 +87,35 @@ function activateForm(){
             answer[i].style.display='block'
         })
 }
-hideAnswers()
-    }
+    for(let n=0;n<answer.length;n++){
+        answer[n].addEventListener('click',function(){
+            answer[n].classList.toggle()
+    })
+}
+}
+/*Meet The Team*/
+function toggleTeamBio(){
+  const  teamMember=document.getElementsByClassName('TeamMemberName')
+     /*const bio=document.createElement('p')*/
 
- function hideAnswers(){
-  answers=document.getElementsByClassName('minus-sign')
-  for(let n=0;n<answers.length;n++){
-    answers[n].addEventListener('click',function(){
-        answer[n].classList.toggle();
-      }
-    )}
-  } 
+     for (let a=0;a<teamMember.length;a++){
+teamMember[a].addEventListener('click',function(){
+ console.log(teamMember[a])
+ const bio=document.createElement('div')
+ bio.classList.add('bio')
+ if(teamMember[a].textContent==='Chris Mwenda'){
+ bio.textContent="Chris is an Entrepreneur by birth. WIth a bachelors degree in business and years long experience in real estate, he is the brains behind this organisation "
+ }
+ if(teamMember[a].textContent==='Willie Mbanya'){
+   bio.textContent="Willie is an product manager per excellence.Over the years he ahs been designing and building attractive looking apps. He's the hipster."
+   }
+ if(teamMember[a].textContent==='Jonathan Munyi'){
+    bio.textContent="Jonathan is the architect, orchestrating how things happen behind the scenes. He's the hacker who makes sure Joomba's engines run so smoothly"
+     }
+ role=document.getElementsByClassName('role')
+ role[a].after(bio)
+ console.log(bio)
 
-
-  function toggleTeamBio(){
-   const  teamMember=document.getElementsByClassName('TeamMemberName')
-      /*const bio=document.createElement('p')*/
-
-      for (let a=0;a<teamMember.length;a++){
- teamMember[a].addEventListener('click',function(){
-  console.log(teamMember[a])
-  const bio=document.createElement('div')
-  bio.classList.add('bio')
-  if(teamMember[a].textContent==='Chris Mwenda'){
-  bio.textContent="Chris is an Entrepreneur by birth. WIth a bachelors degree in business and years long experience in real estate, he is the brains behind this organisation "
-  }
-  if(teamMember[a].textContent==='Willie Mbanya'){
-    bio.textContent="Willie is an product manager per excellence.Over the years he ahs been designing and building attractive looking apps. He's the hipster."
-    }
-  if(teamMember[a].textContent==='Jonathan Munyi'){
-     bio.textContent="Jonathan is the architect, orchestrating how things happen behind the scenes. He's the hacker who makes sure Joomba's engines run so smoothly"
-      }
-  role=document.getElementsByClassName('role')
-  role[a].after(bio)
-  console.log(bio)
- 
-        }
+       }
       )}
-    }
+}
